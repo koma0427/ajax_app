4c72fd8b72b7be7b8c97833263dab6642782e38e
koma0427 <komatsur0427@gmail.com>
2023-02-22 16:58:46 +0900

    テスト

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,16 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+<<<<<<< Updated upstream
   def new
   end
 
   def create
     Post.create(content: params[:content])
+=======
+  def create
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
+>>>>>>> Stashed changes
   end
 end
